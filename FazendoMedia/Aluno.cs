4aeba816ae1e8133388a362193777dc6afac0c2e
Vilberto Junior <149752352+Vilbj1@users.noreply.github.com>
2023-11-03 01:37:16 -0300

    Add files via upload

---
+++ FazendoMedia/Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FazendoMedia
+{
+    public class Aluno
+    {
+        // Aqui estou criando atributos para o nome de um futuro objeto
+        public required string nome;
+
+        public double nota1, nota2;
+
+        //a partir daqui, métodos criados para a média e situação do aluno
+
+        //média
+        public double media()
+        {
+            return (nota1 + nota2)/2;
+        }
+        //situação
+        public string situacao(double media)
+        {
+            return media >= 7 ? "aprovado" : "reprovado";
+        }
+
+        //Mensagem
+        public void mensagem()
+        {
+            //Obtendo a media
+            double obterMedia = media();
+
+            //Obter a situação
+            string obterSituacao = situacao(obterMedia);
+
+            //Mensagem
+            Console.WriteLine(nome + " está " + obterSituacao + " com média " + obterMedia);
+        }
+    }
+}
---
+++ FazendoMedia/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FazendoMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciar objeto da classe Aluno
+            Aluno a = new()
+            {   
+                nome = "Vilberto",
+                nota1 = 5,
+                nota2 = 7
+            };
+            a.mensagem();
+        }
+    }
+}
